6d05d684d5b788c4c24dc8f12544f5f2698af257
Niklas Köhn <n.koehn@esri.de>
2023-12-07 00:17:49 +0100

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 export * from './components';
 
+// TODO: do this inside sketchtopo-cmp
 // settings props like here:  https://gilfink.medium.com/using-complex-objects-arrays-as-props-in-stencil-components-f2d54b093e85
 let sketchtopoCmp = document.querySelector('sketchtopo-component');
-// cmp.checkGeometries = [{"geometry":{"type":"point","x":-0.178,"y":51.48791,"z":1010},"symbol":{"type":"simple-marker","color":[226,119,40],"outline":{"color":[255,255,255],"width":2}}}]
-sketchtopoCmp.checkGeometries = [{"type":"point","x":-0.178,"y":51.48791,"z":1010} as __esri.Point]
-
 let sketchWidget: __esri.Sketch = undefined;
 let sketchCmp = document.querySelector('arcgis-sketch');
 sketchCmp.addEventListener("widgetReady", (evt: CustomEvent<any>) => {
