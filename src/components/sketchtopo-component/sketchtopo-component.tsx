37a0939800dae6e3983302b9756766cec4125b7b
Niklas Köhn <n.koehn@esri.de>
2023-12-07 21:18:51 +0100

    sketch widget within comp: no styles?

--- src/components/sketchtopo-component/sketchtopo-component.tsx
+++ src/components/sketchtopo-component/sketchtopo-component.tsx
@@ -1,9 +1,11 @@
 import { Component, Prop, Watch, h } from '@stencil/core';
 import { Geometry } from '@arcgis/core/geometry';
-import MapView from "@arcgis/core/views/MapView";
-import SceneView from "@arcgis/core/views/SceneView";
-import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
-import Query from "@arcgis/core/rest/support/Query";
+import MapView from '@arcgis/core/views/MapView';
+import SceneView from '@arcgis/core/views/SceneView';
+import FeatureLayer from '@arcgis/core/layers/FeatureLayer';
+import GraphicsLayer from '@arcgis/core/layers/GraphicsLayer';
+import Query from '@arcgis/core/rest/support/Query';
+import Sketch from "@arcgis/core/widgets/Sketch";
 
 // TODO: for some reason, styles are not applied, although they should be => https://stenciljs.com/docs/styling#styling-with-the-shadow-dom. does this interfere with view.ui?
 @Component({
@@ -12,15 +14,14 @@ import Query from "@arcgis/core/rest/support/Query";
   shadow: true,
 })
 export class SketchTopoComponent {
-
   @Prop() checkGeometries: Geometry[];
-  @Prop() position: "bottom-leading"|"bottom-left"|"bottom-right"|"bottom-trailing"|"top-leading"|"top-left"|"top-right"|"top-trailing"|"manual";
+  @Prop() position: 'bottom-leading' | 'bottom-left' | 'bottom-right' | 'bottom-trailing' | 'top-leading' | 'top-left' | 'top-right' | 'top-trailing' | 'manual';
   @Prop() referenceElement: string;
   private view: MapView | SceneView;
   private sketchTopoComponent?: HTMLElement;
   private polygonLayer: FeatureLayer;
   private polygonLayerQuery: Query;
-  
+
   @Watch('checkGeometries')
   async watchGeometries(newValue: Geometry[], oldValue: Geometry[]) {
     console.log('The old value of checkGeometries is: ', oldValue);
@@ -31,37 +32,54 @@ export class SketchTopoComponent {
     this.polygonLayerQuery.geometry = newValue[0];
     this.polygonLayerQuery.spatialRelationship = 'contains';
     const fCount = await this.polygonLayer.queryFeatureCount(this.polygonLayerQuery);
-    this.sketchTopoComponent.innerText =  `Your drawn feature contains ${fCount} features on the layer.`
+    this.sketchTopoComponent.innerText = `Your drawn feature contains ${fCount} features on the layer.`;
   }
 
   private setupView(view: MapView | SceneView) {
     this.view = view;
     this.view.ui.add(this.sketchTopoComponent, this.position);
 
+    const sketchLayer = new GraphicsLayer({ id: 'sketchLayer' });
+    this.view.map.add(sketchLayer);
+    const sketchWidget = new Sketch({
+      layer: sketchLayer,
+      view: this.view
+    });
+    this.view.ui.add(sketchWidget, this.position);
+    sketchWidget.on('create', (c: any) => {
+      if (c.state == 'complete') {
+        console.log(c);
+        this.checkGeometries = [c.graphic.geometry];
+      }
+    });
+
     // TODO: come up with sth better to get a polygon layer.. probably a prop
-    const allFeatureLayers = (this.view.map.allLayers as any).items.filter((l: __esri.Layer) => l.type == "feature")
-    this.view.whenLayerView(allFeatureLayers[0]).then((lv: __esri.FeatureLayerView) => {
-      this.polygonLayer = lv.layer; //.geometryType == 'polygon' ? lv.layer as FeatureLayer : undefined;
-      lv.layer.popupEnabled = false;
-      this.polygonLayerQuery = this.polygonLayer.createQuery();
-    }).catch(() => {
-      this.sketchTopoComponent.innerText =  "No polygon feature layer found."
-    })
+    const allFeatureLayers = (this.view.map.allLayers as any).items.filter((l: __esri.Layer) => l.type == 'feature');
+    this.view
+      .whenLayerView(allFeatureLayers[0])
+      .then((lv: __esri.FeatureLayerView) => {
+        this.polygonLayer = lv.layer; //.geometryType == 'polygon' ? lv.layer as FeatureLayer : undefined;
+        lv.layer.popupEnabled = false;
+        this.polygonLayerQuery = this.polygonLayer.createQuery();
+      })
+      .catch(() => {
+        this.sketchTopoComponent.innerText = 'No polygon feature layer found.';
+      });
   }
 
   // Reference: https://stenciljs.com/docs/component-lifecycle
   // Called every time the component is connected to the DOM. When the component is first connected, this method is called before componentWillLoad.
-  connectedCallback() {    
-    document.querySelector("arcgis-map")?.addEventListener("viewReady", async (event: any) => {
+  connectedCallback() {
+    document.querySelector('arcgis-map')?.addEventListener('viewReady', async (event: any) => {
       this.setupView(event.detail.view);
     });
-    document.querySelector("arcgis-scene")?.addEventListener("viewReady", async (event: any) => {
+    document.querySelector('arcgis-scene')?.addEventListener('viewReady', async (event: any) => {
       this.setupView(event.detail.view);
     });
 
-    console.log("checkGeometries", this.checkGeometries);
-    console.log("position", this.position);
-    console.log("referenceElement", this.referenceElement);
+    console.log('checkGeometries', this.checkGeometries);
+    console.log('position', this.position);
+    console.log('referenceElement', this.referenceElement);
   }
 
   private getText(): string {
@@ -69,6 +87,11 @@ export class SketchTopoComponent {
   }
 
   render() {
-    return <div class="esri-widget" ref={el => this.sketchTopoComponent = el as HTMLElement}>Hello, El! {this.getText()}</div>;
+    return (
+      <div class="esri-widget" ref={el => (this.sketchTopoComponent = el as HTMLElement)}>
+        <div id="sketchDiv" />
+        Hello, El! {this.getText()}
+      </div>
+    );
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 export * from './components';
 
-// TODO: do this inside sketchtopo-cmp
-// settings props like here:  https://gilfink.medium.com/using-complex-objects-arrays-as-props-in-stencil-components-f2d54b093e85
-let sketchtopoCmp = document.querySelector('sketchtopo-component');
-let sketchWidget: __esri.Sketch = undefined;
-let sketchCmp = document.querySelector('arcgis-sketch');
-sketchCmp.addEventListener("widgetReady", (evt: CustomEvent<any>) => {
-    console.log("widgetReady", evt);
-    sketchWidget = evt.detail.widget;
-    sketchWidget.on("create", (c: any) => {
-        if (c.state == "complete") {
-            console.log(c);
-            sketchtopoCmp.checkGeometries = [c.graphic.geometry];
-        }
-    })
-})
+// // TODO: do this inside sketchtopo-cmp
+// // settings props like here:  https://gilfink.medium.com/using-complex-objects-arrays-as-props-in-stencil-components-f2d54b093e85
+// let sketchtopoCmp = document.querySelector('sketchtopo-component');
+// let sketchWidget: __esri.Sketch = undefined;
+// let sketchCmp = document.querySelector('arcgis-sketch');
+// sketchCmp.addEventListener("widgetReady", (evt: CustomEvent<any>) => {
+//     console.log("widgetReady", evt);
+//     sketchWidget = evt.detail.widget;
+//     sketchWidget.on("create", (c: any) => {
+//         if (c.state == "complete") {
+//             console.log(c);
+//             sketchtopoCmp.checkGeometries = [c.graphic.geometry];
+//         }
+//     })
+// })
